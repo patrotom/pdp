6fdaac3d77dfda6ca6fb1fbf1e0aff1d334a515e
Tomas Patro <tomas.patro@gmail.com>
2020-02-25 16:17:00 +0100

    Added documentation comments

--- src/task1.cpp
+++ src/task1.cpp
@@ -8,18 +8,35 @@
 
 using namespace std;
 
+/**
+ * Servers as a placeholder for a solution variables. Solution consists of best
+ * price, edges included in the cut and the vector with nodes that are separated
+ * into two disjoint sets.
+ */
 class Solution {
 public:
+    /**
+     * Default constructor.
+     */
     Solution(double price, vector<pair<int, int>> edges, vector<int> vec) {
         m_price = price;
         m_edges = edges;
         m_vec = vec;
     }
 
+    /**
+     * Returns best price.
+     */
     double getPrice() { return m_price; }
 
+    /**
+     * Returns edges included in the graph cut.
+     */
     vector<pair<int, int>> getEdges() { return m_edges; }
 
+    /**
+     * Returns vector with nodes separated into two disjoint sets.
+     */
     vector<int> getVec() { return m_vec; }
 private:
     double m_price;
@@ -27,8 +44,15 @@ private:
     vector<int> m_vec;
 };
 
+/**
+ * Represents the graph and includes methods to construct the graph and to solve
+ * the minimum exclusion cut.
+ */
 class Graph {
 public:
+    /**
+     * Default constructor.
+     */
     Graph(int n, int k, int b): m_n(n), m_k(k), m_b(b) {
         for (int i = 0; i < n; i++) {
             vector<pair<int, double>> v;
@@ -37,21 +61,39 @@ public:
         m_bestPrice = n * k / 2;
     }
 
+    /**
+     * Returns number of nodes in the graph.
+     */
     int getN() { return m_n; }
 
+    /**
+     * Returns avarage degree of a node.
+     */
     int getK() { return m_k; }
 
+    /**
+     * Returns number of exclusion pairs.
+     */
     int getB() { return m_b; }
 
-    void insertNode(int u, int v, double w) {
+    /**
+     * Inserts edge between nodes u and v with the weight w to the graph.
+     */
+    void insertEdge(int u, int v, double w) {
         m_graph.at(u).push_back(make_pair(v, w));
         m_graph.at(v).push_back(make_pair(u, w));
     }
 
+    /**
+     * Inserts a new exclusion pair between u and v.
+     */
     void insertExclusionPair(int u, int v) {
         m_exclusionPairs[v] = u;
     }
 
+    /**
+     * Solves the exclusion graph cut problem and returns solution.
+     */
     Solution solveProblem() {
         vector<int> vec(m_n, -1);
         vec.at(0) = 0;
@@ -110,12 +152,20 @@ private:
     }
 };
 
+/**
+ * Helper function which splits a string through a whitespace and adds the
+ * results to the vector.
+ */
 template<typename T>
 vector<T> split(const string& line) {
     istringstream is(line);
     return vector<T>(istream_iterator<T>(is), istream_iterator<T>());
 }
 
+/**
+ * Helper function which reads the input from the stdin and constructs a new
+ * graph.
+ */
 Graph constructGraph() {
     string rawInput;
     getline(cin, rawInput);
@@ -126,7 +176,7 @@ Graph constructGraph() {
     for (int i = 0; i < edgesNum; i++) {
         getline(cin, rawInput);
         vector<double> nums = split<double>(rawInput);
-        g.insertNode(int(nums.at(0)), int(nums.at(1)), nums.at(2));
+        g.insertEdge(int(nums.at(0)), int(nums.at(1)), nums.at(2));
     }
 
     for (int i = 0; i < g.getB(); i++) {
@@ -138,6 +188,9 @@ Graph constructGraph() {
     return g;
 }
 
+/**
+ * Helper function which prints a solution in the human readable format.
+ */
 void printSolution(Solution& s) {
     cout << "Best price: " << s.getPrice() << endl;
 
